7097c6c31005dec520ef5a0efd6ad9ceaab5a308
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-07-05 23:01:38 +0100

    Release 0.4.4

--- xandikos/__init__.py
+++ xandikos/__init__.py
@@ -22,5 +22,5 @@
 
 import defusedxml.ElementTree  # noqa: F401 This does some monkey-patching on-load
 
-__version__ = (0, 4, 3)
+__version__ = (0, 4, 4)
 version_string = ".".join(map(str, __version__))
